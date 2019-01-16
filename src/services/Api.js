790b3b86474a6ae806b5cde34773da023dd1231d
Alan Zeng <alanzeng17@gmail.com>
2019-01-16 03:22:54 -0600

    it is your fault

--- src/services/Api.js
+++ src/services/Api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 // rest api calls?
 export default() => {
   return axios.create({
-    baseURL: 'https://2c6c338e092dbe1c0433d3d4fdae1a28.balena-devices.com/',
+    baseURL: 'https://2c6c338e092dbe1c0433d3d4fdae1a28.balena-devices.com/api',
     headers: {'Access-Control-Allow-Origin': '*'}
   })
 }
