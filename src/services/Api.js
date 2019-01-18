901c9c55b30541500703130318567560c228b787
Carson Schubert <carson.schubert14@gmail.com>
2019-01-17 20:46:11 -0600

    Forgot to change api

--- src/services/Api.js
+++ src/services/Api.js
@@ -2,8 +2,8 @@ import axios from 'axios'
 // rest api calls?
 export default() => {
   return axios.create({
-    // baseURL: 'https://2c6c338e092dbe1c0433d3d4fdae1a28.balena-devices.com/api',
-    baseURL: 'http://localhost:8081/api',
+    baseURL: 'https://2c6c338e092dbe1c0433d3d4fdae1a28.balena-devices.com/api',
+    // baseURL: 'http://localhost:8081/api',
     headers: {'Access-Control-Allow-Origin': '*'}
   })
 }
