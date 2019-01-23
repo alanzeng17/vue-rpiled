af0e6440c2429f4192abbdcfef5de15cbffc276b
Alan Zeng <alanzeng17@gmail.com>
2019-01-22 20:22:11 -0600

    get lights working

--- src/components/color/Favorite.vue
+++ src/components/color/Favorite.vue
@@ -69,6 +69,9 @@ export default {
     },
     submit: function(index) {
       alert('clicked!' + index)
+    },
+    async mounted: function() {
+      
     }
   }
 }
---
+++ src/services/ColorService.js
@@ -0,0 +1,5 @@
+import Api from '@/services/Api'
+
+export default {
+
+}
