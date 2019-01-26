10efafbd83db4452ef227e1b2291f20651119cd9
Alan Zeng <alanzeng17@gmail.com>
2019-01-26 15:50:19 -0600

    Revert "Revert "Save, Retrieve, Trigger implemented and connected w/ frontend""
    This reverts commit cc2c99cb4477fec86d358ab8b7cdeba1bede0d1e.
    
    :wq

---
+++ src/components/color/FavoriteColors.vue
@@ -0,0 +1,13 @@
+<template>
+  <swatch :favorites='true'></swatch>
+</template>
+<script>
+import Swatch from '@/components/color/Swatch'
+
+export default {
+  name: 'Saved',
+  components: {
+    Swatch
+  }
+}
+</script>
--- src/services/Api.js
+++ src/services/Api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 // rest api calls?
 export default() => {
   return axios.create({
-    baseURL: process.env.NODE_ENV ? 'https://2c6c338e092dbe1c0433d3d4fdae1a28.balena-devices.com/api' : 'http://localhost:8081/api',
+    baseURL: process.env.NODE_ENV === 'production' ? 'https://2c6c338e092dbe1c0433d3d4fdae1a28.balena-devices.com/api' : 'http://localhost:8081/api',
     headers: {'Access-Control-Allow-Origin': '*'}
   })
 }
