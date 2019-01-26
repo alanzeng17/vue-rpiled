88457d05fc748c15e657fc1c476f56f663e08f6e
Alan Zeng <alanzeng17@gmail.com>
2019-01-26 15:48:13 -0600

    Revert "env var fix"
    This reverts commit dedeadb02d62788eb3e32f1960f3d9f5b69f3234.

--- src/services/Api.js
+++ src/services/Api.js
@@ -2,7 +2,8 @@ import axios from 'axios'
 // rest api calls?
 export default() => {
   return axios.create({
-    baseURL: process.env.NODE_ENV ? 'https://2c6c338e092dbe1c0433d3d4fdae1a28.balena-devices.com/api' : 'http://localhost:8081/api',
+    baseURL: 'https://2c6c338e092dbe1c0433d3d4fdae1a28.balena-devices.com/api',
+    //baseURL: 'http://localhost:8081/api',
     headers: {'Access-Control-Allow-Origin': '*'}
   })
 }
